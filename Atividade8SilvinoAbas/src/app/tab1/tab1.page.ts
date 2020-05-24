6a06e4fcacdbe4214f8f924141c1027413e7bc71
silbapps <silvinobrigido@gmail.com>
2020-05-24 03:26:18 -0300

    initial commit

---
+++ Atividade8SilvinoAbas/src/app/tab1/tab1.page.ts
@@ -0,0 +1,39 @@
+import { Component, AfterContentInit, OnInit, ViewChild, ElementRef } from '@angular/core';
+declare var google;
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit, AfterContentInit {
+  map;
+ @ViewChild('mapElement', {static: true}) mapElement: ElementRef;
+ //@ViewChild(‘Map’, {static: true}) mapElement: ElementRef;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    const position = new google.maps.LatLng(-21.763409, -43.349034);
+
+    const mapOptions = {
+      zoom: 18,
+      center: position,
+      setMyLocationButtonEnabled: true,
+      disableDefaultUI: false
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: position,
+      map: this.map,
+      animation: google.maps.Animation.DROP
+    });
+  }
+ 
+
+  ngAfterContentInit(): void {
+    
+  }
+
+}
