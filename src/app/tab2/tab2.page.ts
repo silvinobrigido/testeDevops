3f87ba2ec938a682c2ba35da42230d3cde33123a
silbapps <silvinobrigido@gmail.com>
2020-05-24 22:34:33 -0300

    feat teste

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,39 @@
+import { Component, AfterContentInit, OnInit, ViewChild, ElementRef } from '@angular/core';
+declare var google;
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page implements OnInit, AfterContentInit {
+  map;
+ @ViewChild('mapElement', {static: true}) mapElement: ElementRef;
+ //@ViewChild(‘Map’, {static: true}) mapElement: ElementRef;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    const position = new google.maps.LatLng(-21.763409, -43.349034);
+
+    const mapOptions = {
+      zoom: 18,
+      center: position,
+      setMyLocationButtonEnabled: true,
+      disableDefaultUI: false
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: position,
+      map: this.map,
+      animation: google.maps.Animation.DROP
+    });
+  }
+ 
+
+  ngAfterContentInit(): void {
+    
+  }
+
+}
